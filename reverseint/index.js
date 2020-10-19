ad08430058486f9ede39fdc235baf9e145975efc
odeane_croney <odeane_croney@yahoo.com>
2020-10-19 20:55:50 +0200

    reverseint

---
+++ reverseint/index.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+// function reverseInt(n) {
+//   let revString = n.toString().split('').reverse().join('')
+
+//   if (n < 0) {
+//     return parseInt(revString) * -1
+//   }
+//     return parseInt(revString)
+// }
+
+
+function reverseInt(n) {
+  let revString = n.toString().split('').reverse().join('')
+
+    return parseInt(revString) * Math.sign(n)
+}
+
+module.exports = reverseInt;
+
